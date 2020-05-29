d785562d6f18c349a3e0a72ab929422079914849
Louis Pilfold <louis@lpil.uk>
2020-05-29 14:08:08 +0100

    v0.1.0

--- mix.exs
+++ mix.exs
@@ -13,20 +13,20 @@ defmodule MixGleam.MixProject do
       package: [
         maintainers: ["Louis Pilfold"],
         licenses: ["Apache-2.0"],
-        links: %{"GitHub" => "https://github.com/gleam-lang/mix_gleam"},
+        links: %{"GitHub" => "https://github.com/gleam-lang/mix_gleam"}
       ]
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-    ]
+    []
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
