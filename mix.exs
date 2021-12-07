356f7bb53ed328b48f402d9947bd5bcb8377a7b9
Tom Calloway <modellurgist@gmail.com>
2021-12-07 09:01:49 +0000

    bump package minor version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixGleam.MixProject do
   def project do
     [
       app: :mix_gleam,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       name: "mix_gleam",
